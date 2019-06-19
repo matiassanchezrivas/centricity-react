3eb542a9aebfc5cd11ae4ea4d147f87bc6daecde
Matías <matiassanchezrivas@gmail.com>
2019-06-19 17:40:46 -0300

    Select tables init

---
+++ src/components/DynamoModal.js
@@ -0,0 +1,46 @@
+import React, { Component } from 'react'
+import { connect } from 'react-redux';
+import DynamoList from './DynamoList'
+
+class Tabla extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+        }
+    }
+
+    handleCloseModal() {
+        this.setState({ openModal: false })
+    }
+
+    componentDidMount() {
+    }
+
+    render() {
+        const { openModal } = this.state;
+        const { tables } = this.props;
+        return (
+            <div>
+                <DynamoList
+                    items={tables}
+                />
+            </div>
+        )
+    }
+}
+
+const mapDispatchToProps = function (dispatch) {
+    return (
+        {
+            // changeLanguaje: (languaje) => dispatch(changeLanguaje(languaje)),
+        }
+    )
+}
+
+const mapStateToProps = function (state) {
+    return {
+        // languaje: state.settings.currentLanguaje,
+    };
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Tabla);
