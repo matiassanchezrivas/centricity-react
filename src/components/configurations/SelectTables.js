22d4f607c3bd5d51918fbaf9862b6a76207dedd2
Matías <matiassanchezrivas@gmail.com>
2019-07-01 10:37:32 -0300

    Immediate reaction after check persistedTables

--- src/components/configurations/SelectTables.js
+++ src/components/configurations/SelectTables.js
@@ -1,21 +1,15 @@
 import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import { Grid, Button } from '@material-ui/core/';
+import { Grid } from '@material-ui/core/';
 import DynamoList from './DynamoList'
 import _ from 'lodash'
 import { axiosConfigurations } from '../../config/axios'
-const useStyles = makeStyles({
-    root: {
-        flexGrow: 1,
-    },
-});
 
 export default function SelectTables(props) {
-    const classes = useStyles();
-    const { all_tables, all_persisted_tables, tableName, cloud_account_id, customer_id } = props;
-
+    const { all_tables, all_persisted_tables, cloud_account_id, customer_id, fetchPersistedTables } = props;
+    const [checked, setChecked] = React.useState([]);
 
     React.useEffect(() => {
+        console.log('useEffect')
         let newChecked = [];
         all_tables.forEach(table => {
             if (_.findIndex(all_persisted_tables, (o) => o.table_name === table && o.active) !== -1) {
@@ -25,24 +19,16 @@ export default function SelectTables(props) {
         setChecked(newChecked)
     }, [all_tables, all_persisted_tables]);
 
-    const [checked, setChecked] = React.useState([]);
 
     const handleToggle = value => async () => {
         const currentIndex = checked.indexOf(value);
-        const newChecked = [...checked];
-        console.log(value)
-
         if (currentIndex === -1) {
             await updateTable(value, cloud_account_id, customer_id, true)
-                .then(() => newChecked.push(value))
-
+                .then(() => fetchPersistedTables(customer_id, cloud_account_id))
         } else {
             await updateTable(value, cloud_account_id, customer_id, false)
-                .then(() => newChecked.splice(currentIndex, 1))
-
+                .then(() => fetchPersistedTables(customer_id, cloud_account_id))
         }
-        console.log(newChecked)
-        setChecked(newChecked);
     };
 
     const updateTable = async (tableName, cloud_account_id, customer_id, active) => {
